b86e332f09e68969d2d94549ad7e1c64ac129aef
FunszeEvil <danielfoley1122@hotmail.com>
2020-04-03 17:06:49 +0100

    initial commit

---
+++ Java/src/ie/tudublin/Main.java
@@ -0,0 +1,24 @@
+
+package ie.tudublin;
+
+import processing.core.PVector;
+
+
+public class Main{
+
+    // public void Class()
+	// {
+	// 	String[] a = {"MAIN"};
+    //     processing.core.PApplet.runSketch( a, new Class());
+    // }
+    
+    public static void main(String[] args)
+	{
+	
+		// Main main = new Main();
+		System.out.println("Hello World");
+	
+			
+	}
+
+}
