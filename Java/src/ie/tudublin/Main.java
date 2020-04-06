5350759af71dd497cb3aa5edeefc844594672165
FunszeEvil <danielfoley1122@hotmail.com>
2020-04-06 11:55:26 +0100

    gameobject

--- Java/src/ie/tudublin/Main.java
+++ Java/src/ie/tudublin/Main.java
@@ -6,17 +6,18 @@ import processing.core.PVector;
 
 public class Main{
 
-    // public void Class()
-	// {
-	// 	String[] a = {"MAIN"};
-    //     processing.core.PApplet.runSketch( a, new Class());
-    // }
+    public void yasc()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new YASC());
+    }
     
     public static void main(String[] args)
 	{
 	
-		// Main main = new Main();
+		Main main = new Main();
 		System.out.println("Hello World");
+		main.yasc();
 	
 			
 	}
---
+++ Java/src/ie/tudublin/YASC.java
@@ -0,0 +1,13 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class YASC extends PApplet
+{
+    public void settings()
+    {
+        size(800,800);
+
+    }
+
+}
